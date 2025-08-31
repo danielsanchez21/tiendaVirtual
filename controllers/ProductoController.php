109773755b811639621efd6ece52a7a326e7fa35
daniel sanchez <daniel.sanchez21@uptc.edu.co>
2025-08-31 17:03:45 -0500

    Add product creation form and backend integration
    Introduce a new product creation form in `pruebaProducto.html` with fields for product details and input validation on the backend using sanitized POST data. Implement JavaScript functionality for submitting product data via AJAX and dynamically loading category options from the backend.

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -13,12 +13,12 @@ class ProductoController extends Controller
     public function actionCrearproducto()
     {
         $producto = new Producto();
-        $producto->nombre = 'Portátil Lenovo';
-        $producto->descrip_producto = 'Laptop de alto rendimiento';
-        $producto->stock = 10;
-        $producto->precio_costo = 2000000;
-        $producto->precio_venta = 2500000;
-        $producto->fk_categoria = 1;
+        $producto->nombre =filter_input(INPUT_POST, 'nombreproducto', FILTER_SANITIZE_STRING);
+        $producto->descrip_producto = filter_input(INPUT_POST, 'descripcion', FILTER_SANITIZE_STRING);
+        $producto->stock = filter_input(INPUT_POST, 'existencias', FILTER_SANITIZE_NUMBER_INT);;
+        $producto->precio_costo = filter_input(INPUT_POST, 'pdtcosto', FILTER_SANITIZE_NUMBER_INT);;
+        $producto->precio_venta = filter_input(INPUT_POST, 'pdtventa', FILTER_SANITIZE_NUMBER_INT);;
+        $producto->fk_categoria = filter_input(INPUT_POST, 'categoria', FILTER_SANITIZE_NUMBER_INT);;
 
         if (!$producto->validate()) {
             throw new \Exception('Error al crear producto');
